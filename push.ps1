52f40e0ffd86ad8e32536d713f37f5ef747c517f
Nga <ngahuynhthispth@gmail.com>
2026-03-31 08:15:16 +0700

    Update Teacher Dashboard and Monitoring

--- push.ps1
+++ push.ps1
@@ -1,4 +1,7 @@
 param ()
+Write-Host "Dang chuan bi code... (Git Add & Commit)" -ForegroundColor Yellow
+git add .
+git commit -m "Update Teacher Dashboard and Monitoring"
 Write-Host "Dang ket noi voi GitHub... Vui long cho cua so dang nhap hien ra..." -ForegroundColor Cyan
 cd c:\Users\Admin\Desktop\UngdungRenTuHocChoHocSinh
 git push -u origin main
